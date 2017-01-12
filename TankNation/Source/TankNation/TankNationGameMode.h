6012566a08ac1096acc091c506881738f10bd040
Srajan Ratti <msfreedom911@gmail.com>
2017-01-12 22:30:47 +0530

    Basic Setup of the Project

---
+++ TankNation/Source/TankNation/TankNationGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TankNationGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKNATION_API ATankNationGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
