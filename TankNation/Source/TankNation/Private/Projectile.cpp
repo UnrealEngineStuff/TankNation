168e2cd09389cb4b25415c51dc3798c23a1d3b44
Srajan Ratti <msfreedom911@gmail.com>
2017-02-18 21:07:44 +0530

    Missed File

---
+++ TankNation/Source/TankNation/Private/Projectile.cpp
@@ -0,0 +1,28 @@
+// Tank Nation Game Develped By Freedom911.For Copyright ask the user MsFreedom911@gmail.com
+
+#include "TankNation.h"
+#include "Projectile.h"
+
+
+// Sets default values
+AProjectile::AProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectile::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ TankNation/Source/TankNation/Public/Projectile.h
@@ -0,0 +1,25 @@
+// Tank Nation Game Develped By Freedom911.For Copyright ask the user MsFreedom911@gmail.com
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class TANKNATION_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectile();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
